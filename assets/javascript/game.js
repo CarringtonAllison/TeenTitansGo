1f2bec943b7963f253f756783f0a5915ed889958
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-10-08 01:50:10 -0700

    Updated Code and CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,80 +2,65 @@ $("document").ready(function () {
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
-    var randomHighNumber = Math.floor(Math.random() * 120) + 19;
-
-    var crystal1 = Math.floor(Math.random() * 12) + 1;
-    var crystal2 = Math.floor(Math.random() * 12) + 1;
-    var crystal3 = Math.floor(Math.random() * 12) + 1;
-    var crystal4 = Math.floor(Math.random() * 12) + 1;
-    var crystal5 = Math.floor(Math.random() * 12) + 1;
-
-    // console.log(crystal1)
-    // console.log(crystal2)
-    // console.log(crystal3)
-    // console.log(crystal4)
-    // console.log(crystal5)
+    var villainHighNumber = Math.floor(Math.random() * 120) + 19;
 
     function reset() {
         totalScore = 0;
-        randomHighNumber = Math.floor(Math.random() * 120) + 19;
-        crystal1 = Math.floor(Math.random() * 12) + 1;
-        crystal2 = Math.floor(Math.random() * 12) + 1;
-        crystal3 = Math.floor(Math.random() * 12) + 1;
-        crystal4 = Math.floor(Math.random() * 12) + 1;
-        crystal5 = Math.floor(Math.random() * 12) + 1;
-        $("#computer-random-text").html(randomHighNumber)
+        villainHighNumber = Math.floor(Math.random() * 120) + 19;
+        titan1 = Math.floor(Math.random() * 12) + 1;
+        titan2 = Math.floor(Math.random() * 12) + 1;
+        titan3 = Math.floor(Math.random() * 12) + 1;
+        titan4 = Math.floor(Math.random() * 12) + 1;
+        titan5 = Math.floor(Math.random() * 12) + 1;
+        $("#villain-random-text").html(villainHighNumber)
         $("#total-score-text").html(totalScore)
+        $("#titan-1").attr("value", titan1)
+        $("#titan-2").attr("value", titan2)
+        $("#titan-3").attr("value", titan3)
+        $("#titan-4").attr("value", titan4)
+        $("#titan-5").attr("value", titan5)
     }
 
     function start() {
         totalScore = 0;
-        randomHighNumber = Math.floor(Math.random() * 120) + 19;
-        crystal1 = Math.floor(Math.random() * 12) + 1;
-        crystal2 = Math.floor(Math.random() * 12) + 1;
-        crystal3 = Math.floor(Math.random() * 12) + 1;
-        crystal4 = Math.floor(Math.random() * 12) + 1;
-        crystal5 = Math.floor(Math.random() * 12) + 1;
-        $("#computer-random-text").html(randomHighNumber);
+        villainHighNumber = Math.floor(Math.random() * 120) + 19;
+        titan1 = Math.floor(Math.random() * 12) + 1;
+        titan2 = Math.floor(Math.random() * 12) + 1;
+        titan3 = Math.floor(Math.random() * 12) + 1;
+        titan4 = Math.floor(Math.random() * 12) + 1;
+        titan5 = Math.floor(Math.random() * 12) + 1;
+        $("#villain-random-text").html(villainHighNumber);
         $("#total-score-text").html(totalScore)
 
+        // adds value attribute with a random number to each titan
+        $("#titan-1").attr("value", titan1)
+        $("#titan-2").attr("value", titan2)
+        $("#titan-3").attr("value", titan3)
+        $("#titan-4").attr("value", titan4)
+        $("#titan-5").attr("value", titan5)
 
-
-
-        // adds value attribute with a random number to each crystal
-        $("#crystal-1").attr("value", crystal1)
-        $("#crystal-2").attr("value", crystal2)
-        $("#crystal-3").attr("value", crystal3)
-        $("#crystal-4").attr("value", crystal4)
-        $("#crystal-5").attr("value", crystal5)
-
-
-        // when crystal is clicked on it saves value to crystal and then adds to totalScore
-        $(".crystal").on("click", function () {
-            $("#total-score-text").html(totalScore);
+        // when titan is clicked on it saves value to titan and then adds to totalScore
+        $(".titan").on("click", function () {
+            console.log($(this).attr("value"));
             savedNumber = parseInt($(this).attr("value"));
             totalScore += savedNumber;
-            console.log($(this).attr("value"));
             console.log(totalScore);
-            
-            
+            $("#total-score-text").html(totalScore);
+
+
             // logic for wins and losses
-            if (totalScore === randomHighNumber) {
+            if (totalScore === villainHighNumber) {
                 wins++;
                 $("#wins-text").html(wins);
                 reset();
-            } else if (totalScore > randomHighNumber) {
+            } else if (totalScore > villainHighNumber) {
                 losses++;
                 $("#losses-text").html(losses);
                 reset();
             }
         });
-
-
     }
-
     start();
-
 });
 
 
@@ -84,16 +69,27 @@ $("document").ready(function () {
 
 
 
+// var titan1 = Math.floor(Math.random() * 12) + 1;
+// var titan2 = Math.floor(Math.random() * 12) + 1;
+// var titan3 = Math.floor(Math.random() * 12) + 1;
+// var titan4 = Math.floor(Math.random() * 12) + 1;
+// var titan5 = Math.floor(Math.random() * 12) + 1;
 
-    // function randomLowNumber() {
+// console.log(titan1)
+// console.log(titan2)
+// console.log(titan3)
+// console.log(titan4)
+// console.log(titan5)
+
+// function randomLowNumber() {
     //     return Math.floor(Math.random() * 12) + 1;
     // }
 
 
 
-    // $(".crystal").attr().on("click", function () {
+    // $(".titan").attr().on("click", function () {
 
-    //     console.log(randomLowNumber())
+        //     console.log(randomLowNumber())
 
     // });
 
@@ -104,7 +100,7 @@ $("document").ready(function () {
     // document.getElementById("wins-text").textContent = wins;
     // document.getElementById("losses-text").textContent = losses;
     // document.getElementById("total-score-text").textContent = totalScore;
-    // document.getElementById("computer-random-text").textContent = randomHighNumber;
+    // document.getElementById("villain-random-text").textContent = villainHighNumber;
 
 // function add (first, second, third, fourth){
 //     return first+second+third+fourth;
